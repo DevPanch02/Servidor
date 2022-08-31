978b2d4a978c57570a3d807fa13ba68b23e500af
DevPanch02 <molinaedison5@gmail.com>
2022-08-30 23:29:49 -0500

    Base de datos

--- app/app/settings.py
+++ app/app/settings.py
@@ -119,12 +119,13 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'notificaciones',
-        'USER': 'edison',
-        'PASSWORD': 'edison.25',
+        'USER': 'postgres',
+        'PASSWORD': 'muisne',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True
     }
 }
 
